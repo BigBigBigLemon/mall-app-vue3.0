83bba6273e2f30a57b3b988f4aa160dcaf4d48b9
Monster <wangshu-134@163.com>
2021-07-14 23:50:47 +0800

    代码拆分

--- src/views/home/Nearby.vue
+++ src/views/home/Nearby.vue
@@ -22,6 +22,7 @@ const useNearbyListEffect = () => {
     const result = await get('/api/shop/hot-list');
     if (result?.errno === 0 && result?.data?.length) {
       nearbyList.value = result.data;
+      console.log(result.data);
     }
   };
   return { nearbyList, getNearbyList };
