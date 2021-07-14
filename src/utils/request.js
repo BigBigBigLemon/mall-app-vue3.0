5a3484dbc21767bcecc3f3f24fff768875b792ec
Monster <wangshu-134@163.com>
2021-07-14 23:57:19 +0800

    封装axios

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
-
-const instance = axios.create({
+// 创建axios实例，配置参数
+const _axios = axios.create({
   baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
   timeout: 10000
 })
 
 export const get = (url, params = {}) => {
   return new Promise((resolve, reject) => {
-    instance.get(url, { params }).then((response) => {
+    _axios.get(url, { params }).then((response) => {
       resolve(response.data)
     }, err => {
       reject(err)
@@ -17,7 +17,7 @@ export const get = (url, params = {}) => {
 
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    instance.post(url, data, {
+    _axios.post(url, data, {
       headers: {
         'Content-Type': 'application/json'
       }
